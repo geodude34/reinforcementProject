3a97fe3d68421eea1156a692c78a368a47997a04
Jordan Williams <jordan@Jordans-MBP.fios-router.home>
2022-08-29 19:01:25 -0400

    backend commit day 1

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,9 @@ module.exports = {
     //   '/api': 'http://localhost:3000'
     // },
     compress: true,
-    port: 8080
+    port: 8080,
+    proxy: {
+      '/addToTeam': 'http://localhost:3000',
+    }
   }
 };
