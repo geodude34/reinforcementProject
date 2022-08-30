2b930e4682f64ba6bab779b54baa71a50ae0e23a
Thomas Ho <tdho.uh@gmail.com>
2022-08-30 09:31:09 -0500

    working on oauth

--- server/router.js
+++ server/router.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const controller = require('./controller');
 const router = express.Router();
-
+const passport = require('passport');
+require('./authRouter');
 //route for logging in via OAuth -> saving user data in user db
-
+router.get(
+  '/google',
+  passport.authentication('google', { scope: ['email', 'profile'] })
+);
 //route saving Pokemon data in teams db
 router.post('/addToTeam', controller.addPokemon, (req, res) => {
   return res.status(200).json(res.locals.addedPokemon);
 });
 //route delete Pokemon data from teams db
 router.delete('/removeFromTeam', controller.removePokemon, (req, res) => {
-  res.sendStatus(200)
+  res.sendStatus(200);
 });
 
 //route to retrieve all Pokemon data from a specific userID in teams db
