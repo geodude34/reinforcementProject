e85ce18225c489d892dd83252c2029924d9a8e3d
Taryn Cunha <taryn.cunha@gmail.com>
2022-08-31 14:23:24 -0400

    container changes

--- client/components/CompareForm.jsx
+++ client/components/CompareForm.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 import { useState } from 'react';
 
-const compareForm = () => {
+const compareForm = (props) => {
+  const { setPokemonTwo } = props;
+
+  const onSubmit = async () => {
+  const pokemonName = document.querySelector('#userInput').value;
+  try{
+    const pokemonName = document.querySelector('#userInput').value
+    console.log('PokemonName', pokemonName)
+    let results = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    console.log('results', results)
+    if(results) {
+      results = results.data
+      const pokemonTwoStats = {
+        name: pokemonName,
+        hp: results.stats[0].base_stat, 
+        attack: results.stats[1].base_stat,
+        defense:results.stats[2].base_stat, 
+        spattack: results.stats[3].base_stat, 
+        spdefense: results.stats[4].base_stat, 
+        speed: results.stats[5].base_stat, 
+        type1: results.types[0].type.name, 
+        type2: results.types[1]?.type.name || null, 
+        image: results.sprites.front_default  
+      }
+      console.log(pokemonOneStats)
+      setPokemonTwo(pokemonTwoStats)
+    }
   return(
     <div className="displayBox">
       <input placeholder='Enter pokemon to compare here...' type="text" id="userInput"/>
--- client/containers/CompareContainer.jsx
+++ client/containers/CompareContainer.jsx
@@ -5,28 +5,7 @@ import CompareForm from '../components/CompareForm.jsx'
 import Types from '../components/Types.jsx'
 
 
-try{
-    const pokemonName = document.querySelector('#userInput').value
-    console.log('PokemonName', pokemonName)
-    let results = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-    console.log('results', results)
-    if(results) {
-      results = results.data
-      const pokemonOneStats = {
-        name: pokemonName,
-        hp: results.stats[0].base_stat, 
-        attack: results.stats[1].base_stat,
-        defense:results.stats[2].base_stat, 
-        spattack: results.stats[3].base_stat, 
-        spdefense: results.stats[4].base_stat, 
-        speed: results.stats[5].base_stat, 
-        type1: results.types[0].type.name, 
-        type2: results.types[1]?.type.name || null, 
-        image: results.sprites.front_default  
-      }
-      console.log(pokemonOneStats)
-      setPokemonOne(pokemonOneStats)
-    }
+t
 const compare = props => {
 
   return (
