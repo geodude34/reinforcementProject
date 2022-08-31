077676f1f6815611c8db6572afec40990b737f6f
Taryn Cunha <taryn.cunha@gmail.com>
2022-08-31 08:28:37 -0400

    header update

--- client/App.jsx
+++ client/App.jsx
@@ -1,29 +1,22 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import SearchContainer from './containers/SearchContainer.jsx';
 import Header from './header.jsx';
 import SearchContainer from './containers/SearchContainer.jsx';
-import CompareContainer from './containers/CompareContainer.jsx';
 import UserTeam from './components/UserTeam.jsx';
+import Login from './Login.jsx';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Header />
-        <SearchContainer />
-        <CompareContainer />
-        
-
-        <UserTeam />
-        {/* <div className='content'>
+      
           <Routes>
-            <Route exact path='/login' element={<Login />} />
-            <Route exact path='/' element={<MainContainer />} />
-            <Route exact path='/compare' element={<Compare />} />
+            <Route exact path='/' element={<Login />} />
+            <Route exact path='/main' element={<MainContainer />} />
             <Route exact path='/team' element={<UserTeam />} />
           </Routes>
-        </div> */}
+      
         <SearchContainer />
       </BrowserRouter>
     </div>
--- client/header.jsx
+++ client/header.jsx
@@ -3,30 +3,28 @@ import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
 
-// const navigate = useNavigate();
+const navigate = useNavigate();
 
-// const home = () => {
-//     navigate('/');
-// }
+const home = () => {
+    navigate('/main');
+}
 
-// const team = () => {
-//     navigate('/team')
-// }
+const team = () => {
+    navigate('/team')
+}
 
-// const logout = () => {
-//     navigate('/login')
-// }
-// const compare = () => {
-//   navigate('/compare')
-// }
+const logout = () => {
+    navigate('/')
+}
+
 
 return (
   <div className='header-container'>
     <h1>Encyclopokeia</h1>
-    {/* <button className="header-component" onClick={home}>Home</button>
+    <button className="header-component" onClick={home}>Home</button>
     <button className="header-component" onClick={team}>Team</button>
     <button className="header-component" onClick={logout}>Logout</button>
-    <button className="header-component" onClick={compare}>Compare</button> */}
+ 
 
   </div>
  
